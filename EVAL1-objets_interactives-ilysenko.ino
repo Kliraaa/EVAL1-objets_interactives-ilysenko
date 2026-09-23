e8fd8c711bf9549b858a7592cf5cb841bf51fb4e
Kliraaa <2472632@cmontmorency.qc.ca>
2026-09-23 16:43:17 -0400

    I didi iiiiit! The code is done, pure data left

--- EVAL1-objets_interactives-ilysenko.ino
+++ EVAL1-objets_interactives-ilysenko.ino
@@ -1,6 +1,6 @@
 #include <Arduino.h> 
 #include <Bounce2.h> 
-#include <Chrono.h> 
+#include <Chrono.h>
 
 Bounce2::Button bouton1; 
 Bounce2::Button bouton2; 
@@ -10,29 +10,34 @@ Bounce2::Button bouton2;
 #define BROCHE_DEL1 5
 #define BROCHE_DEL2 7
 
+
 Chrono chrono1;
 Chrono chrono2;
 
 bool etatDel = LOW;
 bool etatDel2 = LOW;
+bool clignotementActif = false;
 
 void setup()
 {
   Serial.begin(115200);
 
-  bouton1.setPressedState(LOW);               
-  bouton1.attach(BROCHE_BTN1, INPUT_PULLUP);   
-  bouton1.interval(5);                         
-
-  bouton2.setPressedState(LOW);
-  bouton2.attach(BROCHE_BTN2, INPUT_PULLUP);
-  bouton2.interval(5);
-
+  // Configuration de la DEL1
   pinMode(BROCHE_DEL1, OUTPUT);
-  digitalWrite(BROCHE_DEL1, etatDel);
+  digitalWrite(BROCHE_DEL1, LOW);
 
+  // Configuration de la DEL2
   pinMode(BROCHE_DEL2, OUTPUT);
-  digitalWrite(BROCHE_DEL2, etatDel2);
+  digitalWrite(BROCHE_DEL2, LOW);
+
+  // Configuration du bouton1
+  bouton1.attach(BROCHE_BTN1, INPUT_PULLUP);
+  bouton1.setPressedState(LOW);                      
+
+  // Configuration du bouton2
+  bouton2.attach(BROCHE_BTN2, INPUT_PULLUP);
+  bouton2.setPressedState(LOW);
+  
 }
 
 void loop()
@@ -42,30 +47,21 @@ void loop()
 
   if (bouton1.isPressed())
   {
-    etatDel = !etatDel;
-    
-    Serial.print("bouton1 ");
-    if (etatDel == true) {
-      Serial.print(1);
-    } else {
-      Serial.print(0);
-    }
-    Serial.println();
-
-    digitalWrite(BROCHE_DEL1, etatDel);
+    digitalWrite(BROCHE_DEL1, HIGH);
   }
+  else
+    {
+        digitalWrite(BROCHE_DEL1, LOW);
+    }
 
   if (bouton2.pressed()) {
-    etatDel2 = !etatDel2; 
-    
-    Serial.print("bouton2 ");
-    if (etatDel2 == true) {
-      Serial.print(1);
-    } else {
-      Serial.print(0);
-    }
-    Serial.println();
-
+    if(clignotementActif == 0){
+            clignotementActif = 1;
+            etatDel2 = 1;
+        } else {
+            clignotementActif = 0;
+            etatDel2 = 0;
+        }
     digitalWrite(BROCHE_DEL2, etatDel2);
   }
 }
